337c29640311c6dc057b87ac5ad27eaa57c0b0d4
Amir Zuhdi WIbowo <amirzuhdiwibowo123@gmail.com>
2016-08-03 15:03:32 +0700

    Add files via upload

---
+++ penomoran.js
@@ -0,0 +1,14 @@
+﻿<script type="text/javascript">
+var pres = document.getElementsByClassName('blockcode'), pre, code, lines, pl, cl;
+     for (pl = pres.length, p = 0; p < pl; p++) {
+          pre  = pres[p];
+          code = pre.children[0]; 
+          code.innerHTML = code.innerHTML.split("\n").map
+          (function(line) {return '<span class="line">' + line + '</span>';
+        }).join("\n");
+          lines = code.getElementsByClassName('line');
+     for (cl = lines.length, l = 0; l < cl; l++) {
+          lines[l].style.width = pre.scrollWidth + 'px';
+          }
+}
+</script>
